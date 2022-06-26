bddb2e0548e01ab92e6e1e75d0459ba56be0d4a6
AKravchenko <anatolii.kravchenko@burberry.com>
2022-06-26 20:53:55 +0300

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,343 @@
+export default {
+    name: 'Skills',
+    data: {
+        labels: [
+            '2016-10-22',
+            '2017-05-01',
+            '2019-01-01',
+            '2019-05-01',
+            '2020-08-01',
+            '2021-01-01',
+            'Current time'
+        ],
+        datasets: [
+            {
+                label: 'HTML/CSS',
+                type: 'line',
+                fill: false,
+                data: [
+                    45, 71, 73, 75, 77, 80, 80
+                ],
+                borderColor: '#04AA6D',
+                backgroundColor: '#04AA6D'
+            },
+            {
+                label: 'JavaScript',
+                type: 'line',
+                fill: false,
+                data: [
+                    40, 50, 55, 60, 70, 75, 80
+                ],
+                borderColor: 'red',
+                backgroundColor: 'red'
+            },
+            {
+                label: 'TypeScript',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 65, 68, 71, 73, 75
+                ],
+                borderColor: '#3178c6',
+                backgroundColor: '#3178c6'
+            },
+            {
+                label: 'React',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 70, 73, 75, 81, 83
+                ],
+                borderColor: 'rgb(97 218 251)',
+                backgroundColor: 'rgb(97 218 251)'
+            },
+            {
+                label: 'Node.js',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 30, 60, 70, 73
+                ],
+                borderColor: '#84ba64',
+                backgroundColor: '#84ba64'
+            },
+            {
+                label: 'Express',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 35, 65, 75, 80
+                ],
+                borderColor: '#555',
+                backgroundColor: '#555'
+            },
+            {
+                label: 'Koa',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 0, 55, 30
+                ],
+                borderColor: '#eee',
+                backgroundColor: '#eee'
+            },
+            {
+                label: 'Redux',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 60, 60, 65, 65, 85
+                ],
+                borderColor: '#764ABF',
+                backgroundColor: '#764ABF'
+            },
+            {
+                label: 'React Context',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 80, 70, 65, 60
+                ],
+                borderColor: 'rgb(97 218 251)',
+                backgroundColor: 'white'
+            },
+            {
+                label: 'React Router',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 60, 60, 60, 70, 70
+                ],
+                borderColor: '#1e1e1e',
+                backgroundColor: '#1e1e1e'
+            },
+            {
+                label: 'Webpack',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 60, 70, 73, 75, 80
+                ],
+                borderColor: '#8dd6f980',
+                backgroundColor: '#8dd6f980'
+            },
+            {
+                label: 'Gulp',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 60, 40, 25
+                ],
+                borderColor: '#cf4647',
+                backgroundColor: '#cf4647'
+            },
+            {
+                label: 'Sass',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 60, 65, 67, 70, 77
+                ],
+                borderColor: '#c54786',
+                backgroundColor: '#c54786'
+            },
+            {
+                label: 'GraphQL',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 70, 55, 50, 50, 40
+                ],
+                borderColor: '#e10098',
+                backgroundColor: '#e10098'
+            },
+            {
+                label: 'Jest',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 70, 65, 65, 65, 65
+                ],
+                borderColor: '#942d60',
+                backgroundColor: '#942d60'
+            },
+            {
+                label: 'Chia/Enzyme',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 55, 50, 45, 40, 35
+                ],
+                borderColor: 'blue',
+                backgroundColor: 'blue'
+            },
+            {
+                label: 'React Testing Library',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 0, 0, 65
+                ],
+                borderColor: '#942d60',
+                backgroundColor: '#942d60'
+            },
+            {
+                label: 'Mocha',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 0, 55, 30
+                ],
+                borderColor: '#8d6748',
+                backgroundColor: '#8d6748'
+            },
+            {
+                label: 'MongoDB/Mongoose',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 65, 55, 30
+                ],
+                borderColor: '#00684a',
+                backgroundColor: '#00684a'
+            },
+            {
+                label: 'PostgreSQL/Sequelize',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 0, 60, 30
+                ],
+                borderColor: '#02afef',
+                backgroundColor: '#02afef'
+            },
+            {
+                label: 'Handlebars',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 73, 60, 30
+                ],
+                borderColor: '#e61',
+                backgroundColor: '#e61'
+            },
+            {
+                label: 'Ramda/Lodash/FP-TS',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 75, 73, 70, 60, 50
+                ],
+                borderColor: '#849',
+                backgroundColor: '#3492ff'
+            },
+            {
+                label: 'NPM/Yarn',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 75, 73, 70, 75, 80
+                ],
+                borderColor: '#a32323',
+                backgroundColor: '#a32323'
+            },
+            {
+                label: 'Jira',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 0, 0, 55
+                ],
+                borderColor: '#0052cc',
+                backgroundColor: '#0052cc'
+            },
+            {
+                label: 'ESLint',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 50, 55, 55, 58, 65, 65
+                ],
+                borderColor: '#4B32C3',
+                backgroundColor: '#4B32C3'
+            },
+            {
+                label: 'Babel',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 50, 53, 55, 57, 60, 60
+                ],
+                borderColor: 'yellow',
+                backgroundColor: 'yellow'
+            },
+            {
+                label: 'Git/GitHub/GitLab',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 50, 55, 60, 65, 70, 85
+                ],
+                borderColor: '#f14e32',
+                backgroundColor: '#f14e32'
+            },
+            {
+                label: 'Docker',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 30, 40, 45, 45, 45
+                ],
+                borderColor: '#003f8c',
+                backgroundColor: '#FFFFFF'
+            },
+            {
+                label: 'AWS Clouds',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 0, 0, 45
+                ],
+                borderColor: '#ec7211',
+                backgroundColor: '#ec7211'
+            }
+        ]
+    },
+    options: {
+        plugins: {
+            annotation: {
+                annotations: {}
+            },
+            legend: {
+                position: 'bottom',
+                labels: {
+                    padding: 50,
+                    usePointStyle: true
+                }
+            }
+        },
+        layout: {
+            paddingBottom: 50
+        },
+        scales: {
+            x: {
+                ticks: {
+                    beginAtZero: true
+                }
+            },
+            y: {
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 25,
+                    callback: (value: any) => {
+                        if (value === 0) return ``;
+                        if (value < 26) return `Novice`;
+                        if (value < 51) return `Intermediate`;
+                        if (value < 77) return `Advanced`;
+                        if (value < 101) return `Expert`;
+                    }
+                }
+            }
+        }
+    }
+};
